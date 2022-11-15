1791c4be027bacf6426e15433ed52b5e271b3419
ksy <tpdud3256@gmail.com>
2022-11-16 05:12:36 +0900

    pubsub done

--- messages/roomUpdates/roomUpdates.typeDefs.js
+++ messages/roomUpdates/roomUpdates.typeDefs.js
@@ -1,6 +1,12 @@
 import { gql } from "apollo-server";
 
 export default gql`
+    schema {
+        query: Query
+        mutation: Mutation
+        subscription: Subscription
+    }
+
     type Subscription {
         roomUpdates(roomId: Int!, userId: Int!): Message
     }
--- pubsub.js
+++ pubsub.js
@@ -1,5 +1,24 @@
 import { PubSub } from "graphql-subscriptions";
+import { GooglePubSub } from "@axelspringer/graphql-google-pubsub";
+const serviceAccount = require("./credentials.json");
+// const pubsub = new PubSub();
 
-const pubsub = new PubSub();
+const options = {
+    projectId: serviceAccount["project_id"],
+    credentials: {
+        client_email: serviceAccount["client_email"],
+        private_key: serviceAccount["private_key"],
+    },
+};
+
+const commonMessageHandler = (payload) => {
+    const dataBuffer = payload.data || "{}";
+    return JSON.parse(dataBuffer);
+};
+
+const topic2SubName = (topicName) =>
+    `projects/${serviceAccount["project_id"]}/subscriptions/${topicName}`;
+
+const pubsub = new GooglePubSub(options, topic2SubName, commonMessageHandler);
 
 export default pubsub;
